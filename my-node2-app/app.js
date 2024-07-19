ec900bb7f6989d3085a50ac1db4e086866d98841
sumeyyekoruyucu <sumeyyek5534@gmail.com>
2024-07-19 15:25:48 +0200

    app2v2

--- my-node2-app/app.js
+++ my-node2-app/app.js
@@ -7,11 +7,17 @@ const port = process.env.PORT || 4000;
 const hostname = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Fatih Bey!\n');
+  if (req.url.startsWith('/app2')) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, Fatih Bey!\n');
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from my-node2-app!\n');
+  }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/app2`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
